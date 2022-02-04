8eca7ea9f58d0c9e2c39baf258fa4c7b1e8f10e1
William-Quantum <phamphuthien23111999@gmail.com>
2022-02-04 20:32:08 +0700

    Push from Xcode to Github

---
+++ Memorize Game/Memorize Game/Memorize_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize_GameApp.swift
+//  Memorize Game
+//
+//  Created by mac on 2/4/22.
+//
+
+import SwiftUI
+
+@main
+struct Memorize_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Memorize-Game
- 
+ Enjoying with my first game
