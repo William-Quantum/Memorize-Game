1d754a48c6822588f6a9e3279ed88de53a1cf74a
William-Quantum <phamphuthien23111999@gmail.com>
2022-02-09 20:45:07 +0700

    02

--- Memorize Game/Memorize Game/ContentView.swift
+++ Memorize Game/Memorize Game/ContentView.swift
@@ -8,14 +8,61 @@
 import SwiftUI
 
 struct ContentView: View {
+    var emojin: Array = emojin[24]
     var body: some View {
-        Text("Hello William")
-            .padding()
+        HStack {
+            CardView(content: emojin[0] )
+            CardView(content: emojin[1] )
+            CardView(content: emojin[2] )
+            CardView(content: emojin[3] )
+        }
+        .padding(.horizontal)
+        .foregroundColor(.red)
     }
 }
+struct CardView: View {
+    @State var isFaceUp: Bool = true
+    var content: String
+    var body: some View {
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: 20)
+            if isFaceUp {
+                shape.fill().foregroundColor(.white)
+                shape.stroke(lineWidth: 3)
+                Text(content).font(.largeTitle)
+            }
+            else {
+                shape.fill()
+            }
+        }
+        .onTapGesture {
+            isFaceUp = !isFaceUp
+        }
+    }
+        
+}
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
+        ContentView()
+            .preferredColorScheme(.light)
     }
 }
+
